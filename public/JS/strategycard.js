a0023a21e57e3e8a332e19c77f6fa4f2c7a7200d
Jonathan Aner <65676255+jonaner@users.noreply.github.com>
2023-03-30 13:54:26 -0400

    Update strategycard.js

--- public/JS/strategycard.js
+++ public/JS/strategycard.js
@@ -1,5 +1,6 @@
 const rows = ["Hands","8","9", "10", "11", "12", "13", "14","15", "16", "17", "2,2", "3,3", "4,4", "5,5", "6,6", "7,7", "8,8", "9,9", "10,10", "A,A", "A,2", "A,3", "A,4", "A,5","A,6", "A,7", "A,8", "A,9", "A,10"]  
-  // read in strategy card
+const column = ["Hands", "2", "3", "4", "5", "6", "7", "8", "9", "10", "A"]
+// read in strategy card
 var strategy_card = []
 
 function readCsv(){
@@ -10,13 +11,48 @@ function readCsv(){
             strategy_card.push(item)
         })
     });
+    if (strategy_card.length == 0){
+        console.log("Not read in")
+        strategy_card = [
+            ['Hands,2,3,4,5,6,7,8,9,10,A'],
+            ['8','S','S','S','S','S','H','H','H','H','H'],
+            ['9','D','D','D','D','D','H','H','H','H','H'],
+            ['10','D','D','D','D','D','H','H','H','H','D'],
+            ['11','D','D','D','D','D','H','H','D','D','D'],
+            ['12','S','S','S','S','S','H','H','H','H','H'],
+            ['13','S','S','S','S','S','H','H','H','H','H'],
+            ['14','S','S','S','S','S','H','H','H','H','H'],
+            ['15','S','S','S','S','S','H','H','H','S','S'],
+            ['16','S','S','S','S','S','S','H','S','S','S'],
+            ['17','S','S','S','S','S','S','S','S','S','S'],
+            ['2,2','S','P','P','S','S','P','P','H','P','H'],
+            ['3,3','P','S','S','S','P','P','P','P','P','P'],
+            ['4,4','S','S','H','H','H','H','P','H','H','H'],
+            ['5,5','D','D','D','D','D','H','H','H','D','H'],
+            ['6,6','S','S','S','S','S','P','P','P','H','P'],
+            ['7,7','P','S','P','S','P','P','P','P','P','P'],
+            ['8,8','P','P','P','P','P','P','P','P','P','P'],
+            ['9,9','S','S','S','S','S','S','P','P','P','P'],
+            ['10,10','S','S','S','S','S','S','S','S','S','S'],
+            ['A,A','P','P','P','P','P','P','P','P','P','P'],
+            ['A,2','H','H','H','D','H','H','H','H','H','H'],
+            ['A,3','H','D','D','D','H','H','H','H','H','H'],
+            ['A,4','H','D','D','D','D','H','H','H','H','H'],
+            ['A,5','D','D','D','D','D','H','H','H','H','H'],
+            ['A,6','D','D','D','D','D','H','H','H','H','H'],
+            ['A,7','S','S','S','S','S','S','D','D','D','D'],
+            ['A,8','S','S','S','S','S','S','S','S','S','S'],
+            ['A,9','S','S','S','S','S','S','S','S','S','S'],
+            ['A,10','S','S','S','S','S','S','S','S','S','S'],
+          ]
+    }
 }
 /* returns the column for the dealer's card */
 function dealerColumn(card){
     if (card.length == 3 || card[0] == "J"|| card[0] == "Q"|| card[0] == "K"){
-        return strategy_card[0].indexOf(String(10))
+        return column.indexOf(String(10))
     }
-    return strategy_card[0].indexOf(String(card[0]))
+    return column.indexOf(String(card[0]))
 }
 /* returns the row for the player's hand */
 function playerCardRow(cards, total){ 
@@ -61,7 +97,9 @@ function playerCardRow(cards, total){
 
 /*Gets the best decision message accepting array of players cards, total of the cards added up, dealer's first card as a string*/ 
 function getMessage(player, total, dealer){
-    var decision = strategy_card[playerCardRow(player, total)][dealerColumn(dealer[1])]
+    var row = playerCardRow(player, total);
+    var column = dealerColumn(dealer[1])
+    var decision = strategy_card[row][column];
     if (decision == "H"){
         return "Hit"
     }
